acab27556269a18f1b2325a74f0c3b40301fb378
Hesham El Abd <30498915+HeshamElAbd@users.noreply.github.com>
2019-11-01 23:00:30 +0100

    Add files via upload
    The training data feeder is ready

--- TrainingScript.py
+++ TrainingScript.py
@@ -10,6 +10,9 @@ and train a Transformer encoder on it.
 import tensorflow as tf
 import numpy as np
 import pickle
+# define some global parameters:
+batchSize=256
+lengthOfcondString=90
 # load the dataset
 with open("MidiDataSetAsList.pickle","rb") as input_:
     dataSet=pickle.load(input_)
@@ -30,4 +33,38 @@ with open("music2Int.pickle","wb") as output_:
 with open("int2music.pickle","wb") as output_:
     pickle.dump(int2music,output_)
 # Encode bach corpa
-    
+EncodedMusic=np.array([music2int[musicElement] for musicElement in dataSet])
+# prepear a dataset from the EncodedMusic
+dataSet=tf.data.Dataset.from_tensor_slices(EncodedMusic).batch(
+        batch_size=lengthOfcondString+1,drop_remainder=True)
+def mapTrainingTensor(batch):
+    """
+    takes an encoded musical string of length lengthOfcondString+1 and
+    return two strings of length lengthOfcondString, these two strings 
+    represent the input and the target to the model  and are shift by one. 
+    for example, if the input string is [145 67 789 234] the output is two
+    string the first is 145 67 789 and the second 67 789 234. 
+    """
+    inputs=batch[:-1]
+    output_=batch[1:]
+    return inputs,output_
+dataSet=dataSet.map(mapTrainingTensor).shuffle(1000).batch(batchSize)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
